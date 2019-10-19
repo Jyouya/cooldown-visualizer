6be7baf45b358ad3096d14db6ae72980a1099e48
Will <willwest8xp@gmail.com>
2019-10-19 01:35:44 -0400

    fixed some closest resource bugs

--- src/utils/closestResource.js
+++ src/utils/closestResource.js
@@ -14,29 +14,36 @@ export default function closestResource(
   guess
 ) {
   const points = graphIntersections(
-    costGraph(cooldown, timeline, resourceName),
-    resourceGraph(timeline, resourceName)
-  );
+    costGraph(cooldown, timeline, resourceName).map(([x, y]) => [
+      Math.round(x),
+      y
+    ]),
+    resourceGraph(timeline, resourceName).map(([x, y]) => [Math.round(x), y])
+  ).map(x => Math.round(x));
 
   // binary search for the index before cooldown.time
   let low = 0,
     high = points.length;
 
-  if (high === 1) return points[0];
-  
   while (low < high) {
     const mid = (low + high) >>> 1;
     if (points[mid] < cooldown.time) low = mid + 1;
     else high = mid;
   }
 
+  // TODO: round left and right to nearest integer
   const left = points[low - 1],
     right = points[low];
+
+  // if (low === points.length - 1) return points[points.length - 1];
   console.log('points: ', points);
 
   console.log('left: ', left, 'right: ', right);
-
-  if (Math.abs(left - cooldown.time) < Math.abs(right - cooldown.time))
+  if (!left) {
+    return right;
+  } else if (!right) {
+    return left;
+  } else if (Math.abs(left - cooldown.time) < Math.abs(right - cooldown.time))
     return left;
   else return right;
 }
